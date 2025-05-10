e04cac1222599c13c618aa11dd88b67beb5df655
harok <doriantheblue@naver.com>
2025-05-11 01:42:10 +0900

    feat: 초기 echo 서버 구현

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
-package epoll_echo_go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("클라이언트 연결 종료:", err)
+			return
+		}
+		fmt.Printf("받은 메시지: %s", message)
+		conn.Write([]byte(message))
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("서버 시작: 포트 8000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("연결 수락 실패:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
